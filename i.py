c2af0477a5f54017078caedd6d7d75cee71f5ba5
Danielnk10 <144683027+Bruno007k@users.noreply.github.com>
2023-11-07 11:49:13 -0300

    sergio
    aluno

---
+++ i.py
@@ -0,0 +1,3 @@
+for s in rfange(1,5):
+    print("sergio é bonitão")
+print("eu já sei")
---
+++ y.py
@@ -0,0 +1,6 @@
+n = int(input('inicio'))
+n1 = int(input('fim'))
+n2 = int(input('processo'))
+for c in range(n,n1+1,n2):
+    print(c)
+print('fim')
